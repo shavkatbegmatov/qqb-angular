5f57f1d0f0762701eb51df451fb0cbb976850373
shavkatbegmatov <fstudioadm@gmail.com>
2022-04-15 17:06:37 +0500

    ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { DialogComponent } from './dialog/dialog.component';
 import { ApiService } from './services/api.service';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
@@ -11,26 +14,69 @@ import { ApiService } from './services/api.service';
 export class AppComponent implements OnInit {
   title = 'ng';
 
+  displayedColumns: string[] = ['productName', 'category', 'date', 'freshness', 'price', 'comment', 'action'];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
 constructor(private dialog : MatDialog, private api : ApiService){
 
 }
 ngOnInit(): void {
-  this.api
+  this.getAllProducts();
 }
  openDialog() {
     this.dialog.open(DialogComponent, {
-      width:'30%'
-    });
+      width: '30%',
+      minWidth: '350px',
+    }).afterClosed().subscribe(val => {
+      if(val === 'save') {
+        this.getAllProducts();
+      }
+    })
   }
   getAllProducts() {
     this.api.getProduct()
     .subscribe({
       next: (res) => {
-        console.log(res);
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
       },
       error: (err) => {
         alert("Error while feching the Record!");
       }
     })
+  }
+  editProduct(row: any) {
+    this.dialog.open(DialogComponent, {
+      width: '30%',
+      data: row
+    }).afterClosed().subscribe(val => {
+      if(val === 'update') {
+        this.getAllProducts();
+      }
+    })
+  }
+  deleteProduct(id: number) {
+    this.api.deleteProduct(id)
+    .subscribe({
+      next: (res) => {
+        alert("Product deleted successfully");
+        this.getAllProducts();
+      },
+      error: () => {
+        alert("Error while deleting the product!");
+      }
+    })
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,4 +1,5 @@
 import { HttpClient } from '@angular/common/http';
+import { identifierName } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -12,6 +13,12 @@ export class ApiService {
     return this.http.post<any>("http://localhost:3000/productList/", data);
   }
   getProduct() {
-    return this.http.get<any>("http://localhost:3000/productList/")
+    return this.http.get<any>("http://localhost:3000/productList/");
+  }
+  putProduct(data: any, id: number) {
+    return this.http.put<any>("http://localhost:3000/productList/" + id, data);
+  }
+  deleteProduct(id: number) {
+    return this.http.delete<any>("http://localhost:3000/productList/" + id);
   }
 }
